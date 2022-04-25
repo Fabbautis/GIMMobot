2e1834f0e56c4820f3ba717e63418fea0fe67714
FabBautis <FabBaut@gmail.com>
2022-04-24 23:03:43 -0600

    Added a somewhat dynamic way to change the photo with a click of the button
    TODO:
    1. Get a home page working that redirects to tab 2 / new image
    2. Find out how routing and tabs work so that you can remove the bottom tab thing
    3. Start implementing the "take a photo" feature
    4. Find out how to have those photos displayed for the tab 2 page.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -13,6 +13,7 @@ export class Tab2Page {
   text1: string;
   text2: string;
   quoter: string;
+  placement: string = "hi";
   textboxPlacement:Array<number> = [ //0 = TL, 1 = TR, 2 = BL, 3 = BR, 4 = C
     2,
     0,
@@ -44,35 +45,57 @@ export class Tab2Page {
     4,
     3,
     3,
-    4
+    4,
+    3,
+    2,
+    3,
+    1,
+    2,
+    4,
+    0,
+    4,
+    1,
+    0
   ];
 
-  constructor() {}
+  constructor() {
+  }
 
   ionViewDidEnter() {
-    this.pictureID= Math.floor(Math.random()*30);
+    document.getElementById("new-photo").addEventListener("click", (e:Event) => this.newImage());
+    this.newImage();
+  }
+  newImage() {
+    this.pictureID= Math.floor(Math.random()*40);
     this.text1 = 'death'
     this.text2 = 'from above'
     this.quoter = '-yo mama'
     
     switch (this.textboxPlacement[this.pictureID]){
       case 0:
-        document.getElementById('text').classList.add('top-left');
+        document.getElementById('text').classList.replace(this.placement, 'top-left');
+        this.placement = 'top-left';
       break;
       case 1:
-        document.getElementById('text').classList.add('top-right');
+        document.getElementById('text').classList.replace(this.placement, 'top-right');
+        this.placement = 'top-right';
       break;
       case 2:
-        document.getElementById('text').classList.add('bottom-left');
+        document.getElementById('text').classList.replace(this.placement, 'bottom-left');
+        this.placement = 'bottom-left';
       break;
       case 3:
-        document.getElementById('text').classList.add('bottom-right');
+        document.getElementById('text').classList.replace(this.placement, 'bottom-right');
+        this.placement = 'bottom-right';
       break;
       case 4:
-        document.getElementById('text').classList.add('center');
+        document.getElementById('text').classList.replace(this.placement, 'center');
+        this.placement = 'center';
       break;
       default:
-        document.getElementById('text').classList.add('top-right');
+        document.getElementById('text').classList.replace(this.placement, 'top-right');
+        this.placement = 'top-right';
+      break;
     }
   }
   
