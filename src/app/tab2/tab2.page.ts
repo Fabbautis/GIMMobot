983a4685a044dc76fa95b924eb963151afb33852
FabBautis <FabBaut@gmail.com>
2022-04-24 21:41:34 -0600

    Should've pushed this, huh?

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,21 +6,76 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 
+
 export class Tab2Page {
   pictureID: number;
   textOptions: Array<string>;
   text1: string;
   text2: string;
   quoter: string;
+  textboxPlacement:Array<number> = [ //0 = TL, 1 = TR, 2 = BL, 3 = BR, 4 = C
+    2,
+    0,
+    2, 
+    1,
+    4,
+    4,
+    1, 
+    4,
+    3,
+    4,
+    1,
+    2,
+    3,
+    2,
+    0,
+    4,
+    0,
+    0,
+    0,
+    2,
+    3,
+    1,
+    4,
+    2,
+    0,
+    3,
+    1,
+    4,
+    3,
+    3,
+    4
+  ];
 
   constructor() {}
 
   ionViewDidEnter() {
-    this.pictureID= Math.floor(Math.random()*27);
+    this.pictureID= Math.floor(Math.random()*30);
     this.text1 = 'death'
     this.text2 = 'from above'
     this.quoter = '-yo mama'
+    
+    switch (this.textboxPlacement[this.pictureID]){
+      case 0:
+        document.getElementById('text').classList.add('top-left');
+      break;
+      case 1:
+        document.getElementById('text').classList.add('top-right');
+      break;
+      case 2:
+        document.getElementById('text').classList.add('bottom-left');
+      break;
+      case 3:
+        document.getElementById('text').classList.add('bottom-right');
+      break;
+      case 4:
+        document.getElementById('text').classList.add('center');
+      break;
+      default:
+        document.getElementById('text').classList.add('top-right');
+    }
   }
+  
 }
 
 
